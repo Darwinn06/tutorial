35c509c432cf8227e4318cb59f3cb352e488a49b
Darwinn06 <darwinhun48@gmail.com>
2025-05-13 12:49:52 +0200

    hello

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/teszt', function() {
+    return view('teszt');
+});
+Route::get('/teszt', 'App\Http\Controllers\TesztController@teszt');
+
+Route::get('/names', 'App\Http\Controllers\TesztController@names');
